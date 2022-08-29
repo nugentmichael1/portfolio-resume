2c6e38c8ba4a221e2a21711fa2b64e32104e252f
Michael Nugent <NugentMichael@live.com>
2022-08-28 20:00:48 -0700

    Test Heroku Deploy w/ ProcFile @ root

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import languages from "./api/languages.route.js"
 import projects from "./api/projects.route.js"
+import path from "path"
 
 const app = express()
 
@@ -11,5 +12,22 @@ app.use(express.json())
 app.use("/api/v1/languages", languages)
 app.use("/api/v1/projects", projects)
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
+
+// --------------- deployment -------
+
+const __dirname = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    })
+}
+else{
+    app.get("/",(req,res)=>{
+        res.send("API is running..");
+    });
+}
+// --------------- /deployment -------
 
 export default app
