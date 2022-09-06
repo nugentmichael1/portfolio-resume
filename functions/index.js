4d99daac21953f87877eeb0494bf43bcab28a911
Michael Nugent <NugentMichael@live.com>
2022-09-06 11:50:43 -0700

    Firebase initialization

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
