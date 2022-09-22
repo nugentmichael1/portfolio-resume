102e5f75a02248050e863b75aadd52da85dabc1a
Michael Nugent <NugentMichael@live.com>
2022-09-22 09:42:26 -0700

    Updated axios base url (dev vs prod)

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api/v1/",
+    //baseURL: "http://localhost:5000/api/v1/",
+    //baseURL: "https://us-central1-portfolio-688a4.cloudfunctions.net/",
+    baseURL: (process.env.NODE_ENV === 'development') ? process.env.REACT_APP_EMULATOR_FUNCTIONS_URL : process.env.REACT_APP_FIREBASE_FUNCTIONS_URL,
     headers: {
         "Content-type": "application/json"
     }
