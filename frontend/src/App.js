726c4a9d843a629047f08d748b269cb894bb0e83
Michael Nugent <NugentMichael@live.com>
2023-01-23 11:19:07 -0800

    Session Storage Experiment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,16 @@ import Skills from './Pages/Skills'
 import Projects from './Pages/Projects'
 import './CSS/App.css'
 
+//mock example of initial and complete download from backend instead of individual and as needed.  This will reduce api calls (which cost me money if over google's set limit) from regular users.  It will also as challenge nefarious actors who want to abuse my api as they would have to at least clear session data before page refreshes.
+var BackendData = sessionStorage.getItem("data");
+console.log(BackendData);
+if (BackendData === null) {
+  //mock example.  will replace with actual call to backend, which grabs ALL database tables' information
+  console.log("Calling backend for data.")
+  sessionStorage.setItem("data", "Some data")
+  console.log(sessionStorage.getItem("data"))
+}
+
 function App() {
   return (
     <>
