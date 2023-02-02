73d6c8dc08658a4da2319fba2d1e27d151201af1
Michael Nugent <NugentMichael@live.com>
2023-02-02 14:50:27 -0800

    Cleaning

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 const functions = require("firebase-functions");
 
-// The Firebase Admin SDK to access Firestore.
+// The Firebase Admin SDK to access Firestore.  Only used for initialization.
 const admin = require('firebase-admin');
 
 //Express
